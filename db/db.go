4d862a40db3e0f41d0981cb5414fcb083c3ae5db
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2017-07-07 03:51:11 -0300

    use env var to setup database

--- db/db.go
+++ db/db.go
@@ -1,13 +1,31 @@
 package db
 
-import mgo "gopkg.in/mgo.v2"
+import (
+	"fmt"
+	"os"
+
+	mgo "gopkg.in/mgo.v2"
+)
 
 var Db *mgo.Database
 var DbSession *mgo.Session
 
-func DialDB() {
-	DbSession, _ = mgo.Dial("localhost:27017")
-	Db = DbSession.DB("tryout")
+func DialDB() error {
+	url := "localhost:27017"
+	envUrl := os.Getenv("DATABASE_URL")
+	if envUrl != "" {
+		url = envUrl
+	}
+	dialInfo, err := mgo.ParseURL(url)
+	fmt.Printf("%#v", dialInfo)
+	dialInfo.FailFast = true
+	DbSession, err := mgo.DialWithInfo(dialInfo)
+	if err != nil {
+		return err
+	}
+	fmt.Print("xxx")
+	Db = DbSession.DB("tryout_mongo")
+	return nil
 }
 
 func Coll(collection string) *mgo.Collection {
